44f7e91b4a5d571584141d275ba68ff78c99d835
Shu-Feather <154107411+Shu-Feather@users.noreply.github.com>
2025-06-15 01:16:07 +0800

    CausalSelfAttention Modification

--- train_gpt2.py
+++ train_gpt2.py
@@ -8,32 +8,26 @@ class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-
-        # assert config.n_embd % config.n_head == 0
-
+        assert config.n_embd % config.n_head == 0
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
-        self.c_proj.NANOGPT_SCALE_INIT = 1
-
-        # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                                           .view(1, 1, config.block_size, config.block_size))
 
     def forward(self, x):
-        B, T, C = x.size() 
-        
+        B, T, C = x.size()
         qkv = self.c_attn(x)
         q, k, v = qkv.split(self.n_embd, dim=2)
-        
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-
-        y = F.scaled_dot_product_attention(q, k, v, is_causal=True) # flash attention
-
-        y = y.transpose(1, 2).contiguous().view(B, T, C) 
-
-        # output projection
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
         y = self.c_proj(y)
         return y
 
